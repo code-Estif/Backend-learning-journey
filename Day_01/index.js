e9f2162943ac05865fde4a54dfef3b2fe1edfad6
code-Estif <legendestif@gmail.com>
2026-06-26 16:09:34 +0300

    Day-01

---
+++ Day_01/index.js
@@ -0,0 +1,25 @@
+const myname = "Estif";
+const Age = 17;
+
+console.log(myname);
+console.log(Age);
+
+function greeting(myname) {
+    console.log("Hello ", myname)
+}
+
+greeting("ChatGPT");
+
+function IsAdult() {
+    if(Age => 18) {
+        console.log("you are an adult")
+    } else {
+        console.log("you're under 18")
+    }
+}
+
+IsAdult()
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
