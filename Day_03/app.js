693a3cf4fc5ecbfb410ca50e400dfc803ed126c5
code-Estif <legendestif@gmail.com>
2026-06-29 17:41:18 +0300

    Day_02

---
+++ Day_03/app.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const command = process.argv[3];
+const newTask = process.argv[1];
+
+fs.readFile("tasks.json", "utf8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const tasks = JSON.parse(data);
+    console.log("Your Tasks:");
+    tasks.forEach((task, index) => {
+        console.log(`${index + 1}. ${task}`);
+    });
+});
